416572110f593935d97d04f5074d76f4dc2b4a24
Samir David Stevenson <sstevenson32@login-ice-1.pace.gatech.edu>
2025-10-13 14:30:25 -0400

    fix: move camera up slightly to see plane

--- common/controls.cpp
+++ common/controls.cpp
@@ -23,7 +23,7 @@ glm::mat4 getProjectionMatrix(){
 
 
 // Initial position : on +Z
-glm::vec3 position = glm::vec3( 0, 0, 5 ); 
+glm::vec3 position = glm::vec3( 0, 1.5f, 5 ); 
 // Initial horizontal angle : toward -Z
 float horizontalAngle = 3.14f;
 // Initial vertical angle : none
