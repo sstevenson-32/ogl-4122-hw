cfa8888865450c95213900f69de9d50ebd5b1cfe
amarlin77 <aaronmarlin5272@gmail.com>
2025-11-06 10:10:48 -0500

    added member functions and member variables to UAV

--- buzzy_bowl/ECE_UAV.h
+++ buzzy_bowl/ECE_UAV.h
@@ -1,4 +1,5 @@
 #include <thread>
+#include <array>
 
 
 class ECE_UAV {
@@ -17,13 +18,17 @@ private:
 
 public:
 
-    ECE_UAV(float position[3], float velocity[3], float m_acceleration[3]);
+    ECE_UAV(const std::array<float, 3> &position, const std::array<float, 3> &velocity, const std::array<float, 3> &m_acceleration);
 
     void start();
     void stop();
     float distanceToCenter();
-    float getForceMag();
-
-
+    float getForceMag(); // return the magnitude of the force vector
+    float getVelMag(); // return magnitude of velocity vector 
+    void setPosition(const std::array<float,3> &position); // set m_position to this array;
+    void setVelocity(const std::array<float,3> &velocity); // set m_position to this array;
+    void setAcceleration(const std::array<float,3> &acceleration); // set m_position to this array;
+    void update(); // update member variables     
+    bool checkCollision(const ECE_UAV &otherUAV);
     
-}
+};
