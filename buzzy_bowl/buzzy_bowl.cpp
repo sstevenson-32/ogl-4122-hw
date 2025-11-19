32f299767663002222f3dd68c2969b60d5e9816e
Samir David Stevenson <sstevenson32@login-ice-4.pace.gatech.edu>
2025-11-19 11:21:41 -0500

    init: glowing orb in the middle

--- buzzy_bowl/buzzy_bowl.cpp
+++ buzzy_bowl/buzzy_bowl.cpp
@@ -201,6 +201,43 @@ int main( void )
 	glUseProgram(programID);
 	glUniform3f(colorUniform, 0.0f, 1.0f, 0.0f);
 
+	/********************/
+	/*Load in orb object*/
+	/********************/
+	// Read our .obj file
+	std::vector<glm::vec3> orb_vertices;
+	std::vector<glm::vec2> orb_uvs;
+	std::vector<glm::vec3> orb_normals;
+	bool orb_res = loadOBJ("mpm_vol.08_p16.OBJ", orb_vertices, orb_uvs, orb_normals);
+
+	std::vector<unsigned short> orb_indices;
+	std::vector<glm::vec3> orb_indexed_vertices;
+	std::vector<glm::vec2> orb_indexed_uvs;
+	std::vector<glm::vec3> orb_indexed_normals;
+	indexVBO(orb_vertices, orb_uvs, orb_normals, orb_indices, orb_indexed_vertices, orb_indexed_uvs, orb_indexed_normals);
+
+	// Load it into a VBO - Vertex data stored in GPU memory
+	GLuint orb_vertexbuffer;
+	glGenBuffers(1, &orb_vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, orb_vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, orb_indexed_vertices.size() * sizeof(glm::vec3), &orb_indexed_vertices[0], GL_STATIC_DRAW);
+
+	GLuint orb_uvbuffer;
+	glGenBuffers(1, &orb_uvbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, orb_uvbuffer);
+	glBufferData(GL_ARRAY_BUFFER, orb_indexed_uvs.size() * sizeof(glm::vec2), &orb_indexed_uvs[0], GL_STATIC_DRAW);
+
+	GLuint orb_normalbuffer;
+	glGenBuffers(1, &orb_normalbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, orb_normalbuffer);
+	glBufferData(GL_ARRAY_BUFFER, orb_indexed_normals.size() * sizeof(glm::vec3), &orb_indexed_normals[0], GL_STATIC_DRAW);
+
+	// Generate a buffer for the indices as well
+	GLuint orb_elementbuffer;
+	glGenBuffers(1, &orb_elementbuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, orb_elementbuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, orb_indices.size() * sizeof(unsigned short), &orb_indices[0] , GL_STATIC_DRAW);
+
     std::vector<std::unique_ptr<ECE_UAV>> uavs;
     std::vector<float> yardLines = { -50.0f, -25.0f, 0.0f, 25.0f, 50.0f };
 
@@ -289,6 +326,37 @@ int main( void )
 			// 3) Draw the triangles !
 			glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT, (void*)0);
 		};
+
+		// Render our orb
+		{
+            // 1) Set uColor to green
+            glUseProgram(programID);
+            glUniform3f(colorUniform, 1.0f, 1.0f, 0.5f);	//Set emissiv color
+
+			// Position at middle, scale large
+			glm::mat4 ModelMatrixOrb = glm::mat4(1.0);
+			ModelMatrixOrb = glm::translate(ModelMatrixOrb, glm::vec3(1.0f, 50.0f, 0.0f));
+			glm::mat4 MVPOrb = ProjectionMatrix * ViewMatrix * ModelMatrixOrb;
+			glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVPOrb[0][0]);
+			glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrixOrb[0][0]);
+
+			// Bind sphere buffers
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, orb_vertexbuffer);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+			glEnableVertexAttribArray(1);
+			glBindBuffer(GL_ARRAY_BUFFER, orb_uvbuffer);
+			glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+			glEnableVertexAttribArray(2);
+			glBindBuffer(GL_ARRAY_BUFFER, orb_normalbuffer);
+			glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, orb_elementbuffer);
+			glDrawElements(GL_TRIANGLES, orb_indices.size(), GL_UNSIGNED_SHORT, (void*)0);
+		}
+
 
 		// 5) Render the rectangle
 		{
