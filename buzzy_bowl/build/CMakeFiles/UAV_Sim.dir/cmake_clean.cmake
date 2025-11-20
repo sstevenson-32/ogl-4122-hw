ec91ddddeb51efb779249bbababbc1b2721379f0
amarlin77 <aaronmarlin5272@gmail.com>
2025-11-20 08:38:20 -0500

    rendering the uavs

---
+++ buzzy_bowl/build/CMakeFiles/UAV_Sim.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UAV_Sim.dir/link.d"
+  "CMakeFiles/UAV_Sim.dir/ECE_UAV.cpp.o"
+  "CMakeFiles/UAV_Sim.dir/ECE_UAV.cpp.o.d"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/controls.cpp.o"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/controls.cpp.o.d"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/objloader.cpp.o"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/objloader.cpp.o.d"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/shader.cpp.o"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/shader.cpp.o.d"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/texture.cpp.o"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/texture.cpp.o.d"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/vboindexer.cpp.o"
+  "CMakeFiles/UAV_Sim.dir/home/aaron/Documents/School/4122/ogl-4122-hw/common/vboindexer.cpp.o.d"
+  "CMakeFiles/UAV_Sim.dir/main.cpp.o"
+  "CMakeFiles/UAV_Sim.dir/main.cpp.o.d"
+  "UAV_Sim"
+  "UAV_Sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UAV_Sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ buzzy_bowl/build/CMakeFiles/UAV_Sim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UAV_Sim.
+# This may be replaced when dependencies are built.
